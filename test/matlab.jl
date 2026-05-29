ad38c46de36e3ad23a62cee2b33435ba2b1f91f6
Jeff Fessler <JeffFessler@users.noreply.github.com>
2026-05-29 07:26:05 -0700

    Add matlab comparison test (#7)
    * Add matlab comparison test
    
    * add MAT
    
    * raise tol

---
+++ test/matlab.jl
@@ -0,0 +1,45 @@
+#=
+matlab.jl
+Test Julia versus Matlab version
+
+Steps for comparing Matlab to Julia versions
+
+1. git Toppe repo
+git clone git@github.com:toppeMRI/toppe.git
+
+2.
+Launch Matlab
+
+3.
+Add top toppe directory to Matlab path, something like:
+>> addpath ~/dat/git/clone/toppe
+
+4.
+Design an RF pulse with Matlab version and write to a file
+>> slr6 = single(toppe.utils.rf.jpauly.dzrf(100, 6));
+>> plot(slr6)
+>> save slr6.mat slr6
+
+5.
+Run the test below.
+
+For this test case, the maximum of mat6 is 0.059
+and the maximum difference between the Julia and Matlab versions is 9.29e-5
+which is a bit larger than I might have hoped
+but if you plot them they are visually indistinguishable.
+=#
+
+using MAT: matread
+using MRIPulses: dzrf
+using Test: @test, @testset
+
+@testset "matlab" begin
+    mat6 = matread("slr6.mat")["slr6"][:,1]
+    n = 100
+    tb = 6
+    julia6 = dzrf( ; n, tb, ptype = :st, ftype = :ls, )
+    norm = x -> maximum(abs, x) # ∞ norm
+    @show norm(julia6 - mat6)
+    @test isapprox(julia6, mat6; atol = 2e-4, norm)
+    # plot([julia6 mat6]) # visually indistinguishable
+end
